698eb09739e2c6b0006c64c91c2b504769a0a2c2
valentinmadiot <valentin.madiot@gmail.com>
2025-06-02 04:16:17 +0700

    update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,16 +1,38 @@
-import TopBar from "../../components/TopBar/TopBar";
+import { IconChevronUp } from "@tabler/icons-react";
+import { useEffect } from "react";
 import PostShare from "../../components/PostShare/PostShare";
 import Posts from "../../components/Posts/Posts";
+import TopBar from "../../components/TopBar/TopBar";
 import "./home.css";
 
 const Home = () => {
+  useEffect(() => {
+    const scrollUp = () => {
+      const scrollUpElement = document.getElementById("scroll-up");
+      if (!scrollUpElement) return;
+
+      if (window.scrollY >= 350) {
+        scrollUpElement.classList.add("show-scroll");
+      } else {
+        scrollUpElement.classList.remove("show-scroll");
+      }
+    };
+
+    window.addEventListener("scroll", scrollUp);
+
+    return () => window.removeEventListener("scroll", scrollUp);
+  }, []);
+
   return (
     <main className="home">
-        <TopBar />
-        <div className="container">
-          <PostShare />
-          <Posts />
-        </div>
+      <TopBar />
+      <div className="container">
+        <PostShare />
+        <Posts />
+        <a href="#" className="scrollup" id="scroll-up">
+          <IconChevronUp stroke={2} />
+        </a>
+      </div>
     </main>
   );
 };
